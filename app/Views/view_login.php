f74274681b64979d837af12614c71c19a3602ae2
adilsanak <adilsaputra0101@gmail.com>
2021-07-29 09:24:35 +0700

    report

--- app/Views/view_login.php
+++ app/Views/view_login.php
@@ -12,7 +12,7 @@
     <title>Login | Sistem Informasi Pertanian</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= base_url(); ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link rel="shortcut icon" href="<?= base_url(); ?>/assets/img/logo.png">
     <!-- Custom styles for this template-->
